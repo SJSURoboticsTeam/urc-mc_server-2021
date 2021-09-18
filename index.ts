666e7aaf8f549e8ee56f1a43aad209faa223a135
naterpotatoers <nate.garza@hotmail.com>
2021-09-18 00:51:36 -0700

    adds initial swagger documentation page

--- index.ts
+++ index.ts
@@ -1,18 +1,21 @@
-import express from "express";
 import os from "os";
+import express from "express";
+import swaggerUi from "swagger-ui-express";
 
-import { drive } from "./endpoints/drive";
-import { arm } from "./endpoints/arm";
+const swagger = require("./swagger.json");
 
-const app = express().set("json spaces", 2);
+import { drive } from "./routes/drive";
+import { arm } from "./routes/arm";
+
 const port = 5000;
+const app = express().set("json spaces", 2);
 const networkInterfaces = os.networkInterfaces();
 console.log(networkInterfaces);
 
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => res.send("Mission Control Backend"));
+app.use("/", swaggerUi.serve, swaggerUi.setup(swagger));
 app.use("/drive", drive);
 app.use("/arm", arm);
 
