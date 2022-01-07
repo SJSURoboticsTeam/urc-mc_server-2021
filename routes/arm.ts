d962c3a54fde8be7cf20f7b5ee1f0e1282f76a83
acarrou <dridriswim@gmail.com>
2022-01-07 11:57:06 -0800

    arm status and request started

--- routes/arm.ts
+++ routes/arm.ts
@@ -1,6 +1,40 @@
 import express from "express";
-import {} from "../interfaces/arm.interface";
+import { ArmRequest, ArmStatus } from "../interfaces/arm.interface";
 export const arm = express.Router();
+
+var armCommands: ArmRequest = {
+  heartbeat_count: 0,
+  is_operational: 1,
+  arm_speed: 0,
+  rotunda_angle: 0,
+  shoulder_angle: 0,
+  elbow_angle: 0,
+  wrist_roll: 0,
+  wrist_pitch: 0,
+
+  pinky_angle: 0,
+  ring_angle: 0,
+  middle_angle: 0,
+  pointer_angle: 0,
+  thumb_angle: 0,
+}
+
+var armStatus: ArmStatus = {
+  heartbeat_count: 0,
+  is_operational: 1,
+  arm_speed: 0,
+  rotunda_angle: 0,
+  shoulder_angle: 0,
+  elbow_angle: 0,
+  wrist_roll: 0,
+  wrist_pitch: 0,
+
+  pinky_angle: 0,
+  ring_angle: 0,
+  middle_angle: 0,
+  pointer_angle: 0,
+  thumb_angle: 0,
+}
 
 arm.get("/", (req, res) => {
   res.send("arm-endpoint");
